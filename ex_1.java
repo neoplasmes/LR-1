3155dbbf7297c178411ddf878013428dca8a105c
neoplasmes <44320253+neoplasmes@users.noreply.github.com>
2022-11-24 23:24:11 +0300

    Update ex_1.java

--- ex_1.java
+++ ex_1.java
@@ -9,6 +9,7 @@ public class ex_1 {
         int day = 0;
         int month = 0;
         int year = 0;
+        int[] deb = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         Calendar clr = Calendar.getInstance();
         int c_day = clr.get(Calendar.DATE);
@@ -16,34 +17,43 @@ public class ex_1 {
         int c_year = clr.get(Calendar.YEAR);
 
         Scanner src = new Scanner(System.in);
-        try {
-            //день
-            while (true){
-                println("Введите день вашего рождения");
-                day = src.nextInt();
-                println(day);
-                if (day >= 1 & day <= 31){
-                    break;
-                } else { println("так не бывает :/"); }
+        while(true) {
+            try {
+                //день
+                while (true) {
+                    println("Введите день:");
+                    Scanner scr = new Scanner(System.in);
+                    day = scr.nextInt();
+                    if (day >= 1 & day <= 31) {
+
+                        //месяц
+                        println("Введите месяц:");
+                        month = scr.nextInt();
+                        if ((month >= 1 & month <= 12) & (day <= deb[month - 1])) {
+                            break;
+                        } else {
+                            println("так не бывает :/");
+                        }
+
+                    } else {
+                        println("так не бывает :/");
+                    }
+                }
+                //год
+                while (true) {
+                    Scanner scr = new Scanner(System.in);
+                    println("Введите год вашего рождения");
+                    year = src.nextInt();
+                    if (year >= 0 & year <= 2022) {
+                        break;
+                    } else {
+                        println("так не бывает :/");
+                    }
+                }
+                break;
+            } catch (Exception e) {
+                println("Неверный ввод! :(");
             }
-            //месяц
-            while (true){
-                println("Введите месяц вашего рождения");
-                month = src.nextInt();
-                if (month >= 1 & month <= 12){
-                    break;
-                } else { println("так не бывает :/"); }
-            }
-            //год
-            while (true){
-                println("Введите год вашего рождения");
-                year = src.nextInt();
-                if (year >= 0 & year <= 2022){
-                    break;
-                } else { println("так не бывает :/"); }
-            }
-        } catch (Exception e) {
-            println("Неверный ввод! :(");
         }
 
         println("Ваш возраст: " + (c_year - year) + " лет, " + (c_month - month) + " месяцев и " +
